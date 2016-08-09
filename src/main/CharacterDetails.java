0f5504c9d7fb5de7699f37ddb2cb7d965ced4a5f
Sam King <belven000@gmail.com>
2016-08-09 11:58:46 +0100

    Update CharacterDetails.java

--- src/main/CharacterDetails.java
+++ src/main/CharacterDetails.java
@@ -6,6 +6,10 @@ import javax.swing.JTextField;
 public class CharacterDetails extends JPanel {
 	private static final long serialVersionUID = 1L;
 
+	public static int labelColumn = 0;
+	public static int valueColumn = 1;
+	
+	public int ID = 0;
 	public String firstName = "Test";
 	public String secondName = "Name";
 
@@ -13,8 +17,29 @@ public class CharacterDetails extends JPanel {
 	JTextField secondNameTextField = new JTextField(secondName);
 
 	public CharacterDetails() {
-		add(firstNameTextField);
-		add(secondNameTextField);
+		GridBagConstraints constraints = new GridBagConsteraints();
+		setLayout(new GridBagLayout();
+		
+		constraints.gridx = labelColumn;
+		constraints.gridy = 0;
+		constraints.weightx = 0.5;
+		constraints.anchor = GridBagConstraints.WEST;
+		
+		add(new JLabel("First Name"), constraints);
+		
+		constraints.gridx = valueColumn;
+		add(firstNameTextField, constraints);
+		
+		constraints.gridy++;
+		constraints.gridx = labelColumn;
+		add(new JLabel("Second Name"), constraints);
+		
+		constraints.gridx = valueColumn;
+		add(secondNameTextField, constraints);
+		
+		constraints.gridx = labelColumn;
+		constraints.gridy++;
+		add(JSeperator(), constraints);
 	}
 	
 	public void copy(CharacterDetails cd) {
@@ -30,8 +55,10 @@ public class CharacterDetails extends JPanel {
 		repaint();
 	}
 
-	public CharacterDetails(String inFirstName, String inSecondName) {
+	public CharacterDetails(int inID, String inFirstName, String inSecondName) {
 		this();
+		
+		ID = inID;
 		firstName = inFirstName;
 		secondName = inSecondName;
 
